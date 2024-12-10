016a511a13691e7f0ed64318038a603a23e6ab68
tingzhb <tingzhb@gmail.com>
2024-12-10 11:16:24 +0100

    Add Coroutine For MASS Enemy Spawning

--- UnityFundementals/Assets/Scripts/Enemy/EnemySpawner.cs
+++ UnityFundementals/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -6,11 +6,24 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour {
     
 	[SerializeField] GameObject[] enemiesToSpawn;
+	[SerializeField] Transform spawnPoint;
 
 	void OnEnable(){
+		// SpawnEnemies();
+		StartCoroutine(SpawnEnemiesWithDelay()); 
+	}
+	
+	
+	// void SpawnEnemies(){
+	// 	foreach (var enemy in enemiesToSpawn){
+	// 		Instantiate(enemy, spawnPoint.position, Quaternion.identity);
+	// 	}
+	// }
+
+	IEnumerator SpawnEnemiesWithDelay(){
 		foreach (var enemy in enemiesToSpawn){
-			Instantiate(enemy, transform.position, Quaternion.identity);
+			Instantiate(enemy, spawnPoint.position, Quaternion.identity);
+			yield return new WaitForFixedUpdate();
 		}
 	}
-
 }
