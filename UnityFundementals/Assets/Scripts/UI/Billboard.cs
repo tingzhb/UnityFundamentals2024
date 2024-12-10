66c02d958532da63365de13b367742478b48931c
tingzhb <tingzhb@gmail.com>
2024-12-10 14:32:10 +0100

    Add UI Basics

---
+++ UnityFundementals/Assets/Scripts/UI/Billboard.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour{
+	Camera mainCamera;
+
+	void Start(){
+		mainCamera = Camera.main;
+	}
+
+	void LateUpdate(){
+		transform.LookAt(mainCamera.transform);
+		transform.Rotate(0, 180, 0);
+	}
+}
