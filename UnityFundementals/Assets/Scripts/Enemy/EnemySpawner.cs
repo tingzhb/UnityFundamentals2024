6df19d892a80cf3c3b26f6899dee56eec0a160ee
tingzhb <tingzhb@gmail.com>
2024-12-10 11:29:36 +0100

    Add Spawned Enemy List & Despawning Coroutine

--- UnityFundementals/Assets/Scripts/Enemy/EnemySpawner.cs
+++ UnityFundementals/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -8,10 +8,13 @@ public class EnemySpawner : MonoBehaviour {
 	[SerializeField] GameObject[] enemiesToSpawn;
 	[SerializeField] Transform spawnPoint;
 
+	List<GameObject> spawnedEnemies = new();
+
 	void OnEnable(){
 		// SpawnEnemies();
-		StartCoroutine(SpawnEnemiesWithDelay()); 
+		StartCoroutine(SpawnEnemiesWithDelay());
 	}
+	
 	
 	
 	// void SpawnEnemies(){
@@ -22,7 +25,17 @@ public class EnemySpawner : MonoBehaviour {
 
 	IEnumerator SpawnEnemiesWithDelay(){
 		foreach (var enemy in enemiesToSpawn){
-			Instantiate(enemy, spawnPoint.position, Quaternion.identity);
+			var enemyReference = Instantiate(enemy, spawnPoint.position, Quaternion.identity);
+			spawnedEnemies.Add(enemyReference);
+			yield return new WaitForFixedUpdate();
+		}
+		StartCoroutine(DespawnEnemies());
+	}
+
+	IEnumerator DespawnEnemies(){
+		yield return new WaitForSeconds(2);
+		foreach (var spawnedEnemy in spawnedEnemies){
+			Destroy(spawnedEnemy);
 			yield return new WaitForFixedUpdate();
 		}
 	}
